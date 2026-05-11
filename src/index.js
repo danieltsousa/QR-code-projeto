68eaffd90489160c8684718fe80fc7844b578be4
danieltsousa <167568247+danieltsousa@users.noreply.github.com>
2026-05-11 07:47:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
 import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
 
+const QRCode = require('qrcode');
+
+QRCode.toFile('qrcode.png', 'https://exemplo.com', function (err) {
+  if (err) throw err;
+  console.log('QR Code gerado com sucesso!');
+});
+
+
 async function main() {
   prompt.get(promptSchemaMain, async (err, choose) => {
     if (err) console.log(err);
